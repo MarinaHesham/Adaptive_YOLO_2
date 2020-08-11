d7ef2ee357cccfc349222ac25c63182be3fc327b
MarinaHesham <marinahesham21@gmail.com>
2020-08-11 13:31:53 -0400

    add: report latency and mac operations

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ from torchvision import datasets
 from torchvision import transforms
 from torch.autograd import Variable
 import torch.optim as optim
+from ptflops import get_model_complexity_info
 
 
 def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size):
@@ -30,6 +31,9 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
     )
 
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+    detection_time = datetime.timedelta(seconds=0)
+    non_max_suppression_time = datetime.timedelta(seconds=0)
 
     labels = []
     sample_metrics = []  # List of tuples (TP, confs, pred)
@@ -44,10 +48,26 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
         imgs = Variable(imgs.type(Tensor), requires_grad=False)
 
         with torch.no_grad():
+            prev_time = time.time()
             outputs = model(imgs)
+
+            current_time = time.time()
+            inference_time = datetime.timedelta(seconds=current_time - prev_time)
+            prev_time = current_time
+            detection_time += inference_time
+
             outputs = non_max_suppression(outputs, conf_thres=conf_thres, nms_thres=nms_thres)
 
+            current_time = time.time()
+            inference_time = datetime.timedelta(seconds=current_time - prev_time)
+            prev_time = current_time
+            non_max_suppression_time += inference_time
+
         sample_metrics += get_batch_statistics(outputs, targets, iou_threshold=iou_thres)
+
+    print("Detection time is ", detection_time, "Average per image is ", (detection_time/len(dataloader)).microseconds/1000, "ms")
+    print("NMS time is ", non_max_suppression_time, "Average per image is ", (non_max_suppression_time/len(dataloader)).microseconds/1000, "ms")
+    print("Total Inference time is ", detection_time+non_max_suppression_time, "Average per image is ", ((detection_time+non_max_suppression_time)/len(dataloader)).microseconds/1000, "ms")
 
     # Concatenate sample statistics
     true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
@@ -87,6 +107,11 @@ if __name__ == "__main__":
         model.load_state_dict(torch.load(opt.weights_path))
 
     print("Compute mAP...")
+
+    with torch.cuda.device(0):
+        macs, params = get_model_complexity_info(model, (3, 416, 416), as_strings=True, print_per_layer_stat=True, verbose=True)
+        print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+        print('{:<30}  {:<8}'.format('Number of parameters: ', params))
 
     precision, recall, AP, f1, ap_class = evaluate(
         model,
