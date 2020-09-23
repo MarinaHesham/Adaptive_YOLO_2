fca06858d0ff55ab78fa550849546ebfff40ea23
marinahesham <marinahesham21@gmail.com>
2020-09-23 18:51:53 -0400

    rename files

---
+++ test_baseline.py
@@ -0,0 +1,154 @@
+from __future__ import division
+
+from models.models import *
+from utils.utils import *
+from utils.datasets import *
+from utils.parse_config import *
+
+import os
+import sys
+import time
+import datetime
+import argparse
+import tqdm
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from torch.autograd import Variable
+import torch.optim as optim
+from ptflops import get_model_complexity_info
+
+
+def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size):
+    model.eval()
+
+    # Get dataloader
+    dataset = ListDataset(path, img_size=img_size, augment=False, multiscale=False)
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=False, num_workers=1, collate_fn=dataset.collate_fn
+    )
+
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+    detection_time = datetime.timedelta(seconds=0)
+    non_max_suppression_time = datetime.timedelta(seconds=0)
+
+    labels = []
+    sample_metrics = []  # List of tuples (TP, confs, pred)
+    for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc="Detecting objects")):
+
+        # Extract labels
+        labels += targets[:, 1].tolist()
+        # Rescale target
+        targets[:, 2:] = xywh2xyxy(targets[:, 2:])
+        targets[:, 2:] *= img_size
+
+        imgs = Variable(imgs.type(Tensor), requires_grad=False)
+
+        with torch.no_grad():
+            prev_time = time.time()
+            outputs = model(imgs)
+
+            current_time = time.time()
+            inference_time = datetime.timedelta(seconds=current_time - prev_time)
+            prev_time = current_time
+            detection_time += inference_time
+
+            outputs = non_max_suppression(outputs, conf_thres=conf_thres, nms_thres=nms_thres)
+
+            current_time = time.time()
+            inference_time = datetime.timedelta(seconds=current_time - prev_time)
+            prev_time = current_time
+            non_max_suppression_time += inference_time
+
+        sample_metrics += get_batch_statistics(outputs, targets, iou_threshold=iou_thres)
+
+    print("Detection time is ", detection_time, "Average per image is ", (detection_time/len(dataloader)).microseconds/1000, "ms")
+    print("NMS time is ", non_max_suppression_time, "Average per image is ", (non_max_suppression_time/len(dataloader)).microseconds/1000, "ms")
+    print("Total Inference time is ", detection_time+non_max_suppression_time, "Average per image is ", ((detection_time+non_max_suppression_time)/len(dataloader)).microseconds/1000, "ms")
+
+    # Concatenate sample statistics
+    true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
+    precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
+
+    return precision, recall, AP, f1, ap_class
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
+    parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
+    parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
+    parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
+    parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
+    parser.add_argument("--iou_thres", type=float, default=0.3, help="iou threshold required to qualify as detected")
+    parser.add_argument("--conf_thres", type=float, default=0.5, help="object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.3, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    opt = parser.parse_args()
+    print(opt)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    data_config = parse_data_config(opt.data_config)
+    valid_path = data_config["valid"]
+    class_names = load_classes(data_config["names"])
+
+    # Initiate model
+    model = Darknet(opt.model_def).to(device)
+    if opt.weights_path.endswith(".weights"):
+        # Load darknet weights
+        model.load_darknet_weights(opt.weights_path)
+    else:
+        # Load checkpoint weights
+        model.load_state_dict(torch.load(opt.weights_path))
+
+    print("Compute mAP...")
+
+    with torch.cuda.device(0):
+        macs, params = get_model_complexity_info(model, (3, 416, 416), as_strings=True, print_per_layer_stat=True, verbose=True)
+        print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+        print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+
+    if opt.iou_thres == -1:
+        ap5_95 = []
+        for iou_thres in np.arange(0.5,0.95,0.05):
+            print("Evaluating at IOU_thres = ", iou_thres)
+            precision, recall, AP, f1, ap_class = evaluate(
+                model,
+                path=valid_path,
+                iou_thres=iou_thres,
+                conf_thres=opt.conf_thres,
+                nms_thres=opt.nms_thres,
+                img_size=opt.img_size,
+                batch_size=opt.batch_size,
+            )
+
+            print("Average Precisions:")
+            for i, c in enumerate(ap_class):
+                print(f"+ Class '{c}' ({class_names[c]}) - AP: {AP[i]}")
+
+            print(f"mAP: {AP.mean()}")
+            ap5_95.append(AP.mean())
+        print(ap5_95, "mAP(0.5:0.95) = ", sum(ap5_95)/len(ap5_95))
+
+    else:
+        precision, recall, AP, f1, ap_class = evaluate(
+            model,
+            path=valid_path,
+            iou_thres=opt.iou_thres,
+            conf_thres=opt.conf_thres,
+            nms_thres=opt.nms_thres,
+            img_size=opt.img_size,
+            batch_size=opt.batch_size,
+        )
+
+        print("Average Precisions:")
+        for i, c in enumerate(ap_class):
+            print(f"+ Class '{c}' ({class_names[c]}) - AP: {AP[i]}")
+
+        print(f"mAP: {AP.mean()}")
+
---
+++ train_adaptive2.py
@@ -0,0 +1,236 @@
+from __future__ import division
+
+from models.ada_models import *
+from models.models import Darknet
+from utils.logger import *
+from utils.utils import *
+from utils.datasets import *
+from utils.parse_config import *
+from ada_test import evaluate, evaluate_branch
+
+from terminaltables import AsciiTable
+
+import os
+import sys
+import time
+import datetime
+import argparse
+
+import torch, torch.nn as nn
+
+from torch.utils.data import DataLoader, Sampler
+from torchvision import datasets
+from torchvision import transforms
+from torch.autograd import Variable
+import torch.optim as optim
+
+from ptflops import get_model_complexity_info
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
+    parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
+    parser.add_argument("--gradient_accumulations", type=int, default=1, help="number of gradient accums before step")
+    parser.add_argument("--branch_def", type=str, default="config/yolov3-tiny.cfg", help="path to model definition file")
+    parser.add_argument("--backbone_def", type=str, default="config/yolov3-tiny.cfg", help="path to backbone model definition file")
+    parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
+    parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
+    parser.add_argument("--backbone_weights", type=str, help="backbone model weights, must be specified")
+    parser.add_argument("--backbone_num_of_layers", type=str, help="number of layers in backbone model, must be specified")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    parser.add_argument("--checkpoint_interval", type=int, default=100, help="interval between saving model weights")
+    parser.add_argument("--evaluation_interval", type=int, default=1, help="interval evaluations on validation set")
+    parser.add_argument("--compute_map", default=False, help="if True computes mAP every tenth batch")
+    parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
+    parser.add_argument("--frozen_pretrained_layers", type = int, default=-1, help="number of the front layers that should be loaded from pretrained weights")
+    parser.add_argument("--clusters_path", type=str, default="clusters.data", help="clusters file path")
+    parser.add_argument("--cluster_index", type=int, default=0, help="Branch cluster index")
+    parser.add_argument("--ckpt_prefix", type=str, default="", help="pre for checkpoints files")
+
+    opt = parser.parse_args()
+    logger = Logger("logs")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    os.makedirs("output", exist_ok=True)
+    os.makedirs("checkpoints", exist_ok=True)
+
+    # Get data configuration
+    data_config = parse_data_config(opt.data_config)
+    train_path = data_config["train"]
+    valid_path = data_config["valid"]
+    class_names = load_classes(data_config["names"])
+    num_classes = int(data_config["classes"])
+
+    # 1. Create Backbone Model
+    backbone = Backbone(opt.backbone_def).to(device)
+    print("Bachbone Number of Parameters")
+    count_parameters(backbone)
+    
+    # 2. Load Backbone Weights
+    if opt.backbone_weights:
+        backbone.load_darknet_weights(opt.backbone_weights, int(opt.backbone_num_of_layers))
+
+    # 3. Create Branch Model
+    branch = Darknet(opt.branch_def).to(device)
+    print("Branch Number of Parameters")
+    count_parameters(branch)
+
+    branch.apply(weights_init_normal)
+
+    # If specified we start from checkpoint
+    if opt.pretrained_weights:
+        if opt.pretrained_weights.endswith(".pth"):
+            branch.load_state_dict(torch.load(opt.pretrained_weights))
+        else:
+            branch.load_darknet_weights(opt.pretrained_weights,opt.frozen_pretrained_layers)
+
+    
+    optimizer = torch.optim.Adam(branch.parameters())
+
+    metrics = [
+        "grid_size",
+        "loss",
+        "x",
+        "y",
+        "w",
+        "h",
+        "conf",
+        "cls",
+        "cls_acc",
+        "recall50",
+        "recall75",
+        "precision",
+        "conf_obj",
+        "conf_noobj",
+    ]
+
+    ############## READ Clusters file and Create mapping ##########
+    clusters = parse_clusters_config(opt.clusters_path)
+    class_to_cluster_list = get_class_to_cluster_map(clusters)
+
+    # with torch.cuda.device(0):
+    #     macs, params = get_model_complexity_info(branch, (3, 416, 416), as_strings=True, print_per_layer_stat=True, verbose=True)
+    #     print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+    #     print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+
+    best_model = branch
+    best_map = 0
+
+    for epoch in range(opt.epochs):
+        dataset = ListDataset(train_path, augment=True, multiscale=opt.multiscale_training)
+        
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=opt.batch_size,
+            shuffle=True,
+            num_workers=opt.n_cpu,
+            pin_memory=True,
+            collate_fn=dataset.collate_fn,
+        )
+
+        branch.train()
+        start_time = time.time()
+        for batch_i, (_, imgs, targets) in enumerate(dataloader):
+            batches_done = len(dataloader) * epoch + batch_i
+
+            imgs = Variable(imgs.to(device))
+            targets = Variable(targets.to(device), requires_grad=False)
+
+            # 4. Filter data and Convert labels
+            # Convert the original labels to internal cluster based labels
+            targets = map_labels_to_cluster(targets, clusters, class_to_cluster_list, opt.cluster_index, device)
+            if targets.shape[0] == 0:
+                continue
+
+            # 5. Execute Backbone
+            backbone_out = backbone(imgs)
+
+            # 6. pass output of backbone to branch model for training
+            print("LAYER OUTPUTS", len(backbone.layer_outputs))
+            loss, outputs = branch(backbone_out, img_dim=imgs.shape[2], targets=targets, layer_outputs=backbone.layer_outputs)
+            backbone.layer_outputs = []
+
+            if outputs == None:
+                continue
+            loss.backward()
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # ----------------
+            #   Log progress
+            # ----------------
+
+            log_str = "\n---- [Cluster %d, Epoch %d/%d, Batch %d/%d] ----\n" % (opt.cluster_index, epoch, opt.epochs, batch_i, len(dataloader))
+
+            metric_table = [["Metrics", *[f"YOLO Layer {i}" for i in range(len(branch.yolo_layers))]]]
+
+            # Log metrics at each YOLO layer
+            for i, metric in enumerate(metrics):
+                formats = {m: "%.6f" for m in metrics}
+                formats["grid_size"] = "%2d"
+                formats["cls_acc"] = "%.2f%%"
+                row_metrics = [formats[metric] % yolo.metrics.get(metric, 0) for yolo in branch.yolo_layers]
+                metric_table += [[metric, *row_metrics]]
+
+                # Tensorboard logging
+                tensorboard_log = []
+                for j, yolo in enumerate(branch.yolo_layers):
+                    for name, metric in yolo.metrics.items():
+                        if name != "grid_size":
+                            tensorboard_log += [(f"{name}_{j+1}", metric)]
+                tensorboard_log += [("loss", loss.item())]
+                logger.list_of_scalars_summary(tensorboard_log, batches_done)
+
+            log_str += AsciiTable(metric_table).table
+            log_str += f"\nTotal loss {loss.item()}"
+
+            # Determine approximate time left for epoch
+            epoch_batches_left = len(dataloader) - (batch_i + 1)
+            time_left = datetime.timedelta(seconds=epoch_batches_left * (time.time() - start_time) / (batch_i + 1))
+            log_str += f"\n---- ETA {time_left}"
+
+            print(log_str)
+
+            branch.seen += imgs.size(0)
+
+        torch.save(branch.state_dict(), f"checkpoints/%s_yolov3_ckpt_clus%d_%d.pth" %(opt.ckpt_prefix, opt.cluster_index, epoch))
+
+        print(f"\n---- Evaluating Model on Cluster ----", opt.cluster_index)
+        # Evaluate the model on the validation set
+        precision, recall, AP, f1, ap_class = evaluate_branch(
+            backbone,
+            branch,
+            path=valid_path,
+            iou_thres=0.5,
+            conf_thres=0.01,
+            nms_thres=0.5,
+            img_size=opt.img_size,
+            batch_size=1,
+            clusters_path=opt.clusters_path,
+            clusters_idx=opt.cluster_index,
+            device=device
+        )
+        evaluation_metrics = [
+            ("val_precision", precision.mean()),
+            ("val_recall", recall.mean()),
+            ("val_mAP", AP.mean()),
+            ("val_f1", f1.mean()),
+        ]
+        logger.list_of_scalars_summary(evaluation_metrics, epoch)
+
+        # Print class APs and mAP
+        ap_table = [["Index", "Class name", "AP"]]
+        for i, c in enumerate(ap_class):
+            ap_table += [[c, class_names[c], "%.5f" % AP[i]]]
+        print(AsciiTable(ap_table).table)
+        print(f"---- mAP {AP.mean()}")
+        if AP.mean() > best_map:
+            best_map = AP.mean()
+            best_model = branch
+
+    print("Saving best model of mAP", best_map)
+
+    best_model.save_darknet_weights("weights/%s_yolov3_ada.weights" % opt.ckpt_prefix)
+    torch.save(best_model.state_dict(), f"checkpoints/%s_yolov3_ada.pth" % opt.ckpt_prefix)
