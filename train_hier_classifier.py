bcc0835c1bca0a0203f3fdabe39e259f9aa877f8
MarinaHesham <marinahesham21@gmail.com>
2020-09-23 14:57:49 -0400

    enable testing only for hier classifier

--- train_hier_classifier.py
+++ train_hier_classifier.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
-    parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
+    parser.add_argument("--gradient_accumulations", type=int, default=1, help="number of gradient accums before step")
     parser.add_argument("--model_def", type=str, default="config/yolov3-tiny.cfg", help="path to model definition file")
     parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
     parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
@@ -53,6 +53,7 @@ if __name__ == "__main__":
     parser.add_argument("--clusters_path", type=str, default="clusters.data", help="clusters file path")
     parser.add_argument("--ckpt_prefix", type=str, default="", help="pre for checkpoints files")
     parser.add_argument("--ckpt_postfix", type=str, default="", help="post for checkpoints files")
+    parser.add_argument("--test_only", type=bool, default=False, help="only test model")
 
 
     opt = parser.parse_args()
@@ -95,71 +96,8 @@ if __name__ == "__main__":
 
     count_parameters(model)
 
-    dataset = ClustersDataset(train_path, augment=True, multiscale=opt.multiscale_training,  clusters=clusters)
-    
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=opt.batch_size,
-        shuffle=True,
-        num_workers=opt.n_cpu,
-        pin_memory=True,
-        collate_fn=dataset.collate_fn
-    )
-
-    criterion = nn.MSELoss()
-    learning_rate = 0.001
-    momentum = 0.9
-    random_seed = 1
-    torch.manual_seed(random_seed)
-
-    best_accuracy = 0
-    best_model = model
-
-    optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate, momentum=momentum)
-    print(model.parameters) 
-    for epoch in range(opt.epochs):
-        model.train()
-        start_time = time.time()
-        
-        for batch_i, (_, imgs, targets) in enumerate(dataloader):
-            imgs = Variable(imgs.to(device))
-            targets = Variable(targets.to(device), requires_grad=False)
-            output = model(imgs,[])
-            #print(output)
-            loss =  criterion(output, targets)
-            loss.backward()
-            
-            if batch_i % 1 == 0:
-                optimizer.step()    # Does the update
-                optimizer.zero_grad()
-            
-            model.seen += imgs.size(0)
-            if batch_i % 100 == 0:
-                print(batch_i,len(dataloader), loss.cpu().detach().numpy())
-        torch.save(model.state_dict(), f"checkpoints/%s_yolov3_cluster_net_%s_%d.pth" % (opt.ckpt_prefix, opt.ckpt_postfix, epoch))
-        '''
-        print("Evaluate on ", len(dataloader))
-
-        right_predictions = 0
-        all_predictions = 0
-        
-        for batch_i, (_, imgs, targets) in enumerate(dataloader):
-            model.eval()
-            imgs = Variable(imgs.to(device))
-            targets = Variable(targets.to(device), requires_grad=False)
-
-            output = model(imgs)
-            loss = criterion(output, targets)
-
-            output = torch.argmax(output.cpu(), dim=1).numpy()
-            targets = torch.argmax(targets.cpu(), dim=1).numpy()
-            right_predictions += np.count_nonzero(targets==output)
-            all_predictions += len(targets)
-
-        print(epoch, "Accuracy on Training = ", 100.0*right_predictions/all_predictions)
-        '''
+    if opt.test_only:
         dataset_valid = ClustersDataset(valid_path, augment=True, multiscale=False,  clusters=clusters)
-
         dataloader_valid = torch.utils.data.DataLoader(
             dataset_valid,
             batch_size=opt.batch_size,
@@ -179,7 +117,6 @@ if __name__ == "__main__":
             targets = Variable(targets.to(device), requires_grad=False)
 
             output = model(imgs,[])
-            loss = criterion(output, targets)
 
             output = torch.argmax(output.cpu(), dim=1).numpy()
             targets = torch.argmax(targets.cpu(), dim=1).numpy()
@@ -187,10 +124,107 @@ if __name__ == "__main__":
             right_predictions += np.count_nonzero(targets==output)
             all_predictions += len(targets)
             
-        print(epoch, "Accuracy of Validation = ", 100.0*right_predictions/all_predictions)
+        print("Accuracy of Validation = ", 100.0*right_predictions/all_predictions)
         if best_accuracy < right_predictions/all_predictions:
             best_accuracy = right_predictions/all_predictions
             best_model = model
-    
-    print("Saving best model with accuracy", best_accuracy)
-    torch.save(best_model.state_dict(), f"checkpoints/%s_yolov3_cluster_net_%s.pth" % (opt.ckpt_prefix, opt.ckpt_postfix))
+
+    else:
+        dataset = ClustersDataset(train_path, augment=True, multiscale=opt.multiscale_training,  clusters=clusters)
+        
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=opt.batch_size,
+            shuffle=True,
+            num_workers=opt.n_cpu,
+            pin_memory=True,
+            collate_fn=dataset.collate_fn
+        )
+
+        criterion = nn.MSELoss()
+        learning_rate = 0.001
+        momentum = 0.9
+        random_seed = 1
+        torch.manual_seed(random_seed)
+
+        best_accuracy = 0
+        best_model = model
+
+        optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate, momentum=momentum)
+        print(model.parameters) 
+        for epoch in range(opt.epochs):
+            model.train()
+            start_time = time.time()
+            
+            for batch_i, (_, imgs, targets) in enumerate(dataloader):
+                imgs = Variable(imgs.to(device))
+                targets = Variable(targets.to(device), requires_grad=False)
+                output = model(imgs,[])
+                #print(output)
+                loss =  criterion(output, targets)
+                loss.backward()
+                
+                if batch_i % 1 == 0:
+                    optimizer.step()    # Does the update
+                    optimizer.zero_grad()
+                
+                model.seen += imgs.size(0)
+                if batch_i % 100 == 0:
+                    print(batch_i,len(dataloader), loss.cpu().detach().numpy())
+            torch.save(model.state_dict(), f"checkpoints/%s_yolov3_cluster_net_%s_%d.pth" % (opt.ckpt_prefix, opt.ckpt_postfix, epoch))
+            '''
+            print("Evaluate on ", len(dataloader))
+
+            right_predictions = 0
+            all_predictions = 0
+            
+            for batch_i, (_, imgs, targets) in enumerate(dataloader):
+                model.eval()
+                imgs = Variable(imgs.to(device))
+                targets = Variable(targets.to(device), requires_grad=False)
+
+                output = model(imgs)
+                loss = criterion(output, targets)
+
+                output = torch.argmax(output.cpu(), dim=1).numpy()
+                targets = torch.argmax(targets.cpu(), dim=1).numpy()
+                right_predictions += np.count_nonzero(targets==output)
+                all_predictions += len(targets)
+
+            print(epoch, "Accuracy on Training = ", 100.0*right_predictions/all_predictions)
+            '''
+            dataset_valid = ClustersDataset(valid_path, augment=True, multiscale=False,  clusters=clusters)
+
+            dataloader_valid = torch.utils.data.DataLoader(
+                dataset_valid,
+                batch_size=opt.batch_size,
+                shuffle=True,
+                num_workers=opt.n_cpu,
+                pin_memory=True,
+                collate_fn=dataset_valid.collate_fn
+            )
+
+            print("Evaluate on ", len(dataloader_valid))
+
+            right_predictions = 0
+            all_predictions = 0
+            for batch_i, (_, imgs, targets) in enumerate(dataloader_valid):
+                model.eval()
+                imgs = Variable(imgs.to(device))
+                targets = Variable(targets.to(device), requires_grad=False)
+
+                output = model(imgs,[])
+
+                output = torch.argmax(output.cpu(), dim=1).numpy()
+                targets = torch.argmax(targets.cpu(), dim=1).numpy()
+
+                right_predictions += np.count_nonzero(targets==output)
+                all_predictions += len(targets)
+                
+            print(epoch, "Accuracy of Validation = ", 100.0*right_predictions/all_predictions)
+            if best_accuracy < right_predictions/all_predictions:
+                best_accuracy = right_predictions/all_predictions
+                best_model = model
+        
+        print("Saving best model with accuracy", best_accuracy)
+        torch.save(best_model.state_dict(), f"checkpoints/%s_yolov3_cluster_net_%s.pth" % (opt.ckpt_prefix, opt.ckpt_postfix))
